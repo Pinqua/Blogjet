e6f4f0402914cb6167f89a90eb2c7ba35c402033
Piyush Sati <piyush.sati@getmyparking.com>
2022-01-15 23:18:51 +0530

    modified code

--- MyProjects/settings.py
+++ MyProjects/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
 
-ALLOWED_HOSTS = ['https://blogjet.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
